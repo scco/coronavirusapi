618f702307172c7eb09194742d20bb05db8abd8a
Caleb Brown <calebcarltonbrown@gmail.com>
2020-03-26 11:08:32 -0400

    Removing redundant line

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,7 @@
 class StaticPagesController < ApplicationController
   HOUR = 3600
   def show
-    @updated_date_timestamp = Time.at((State.all.where('official_flag is true').order(crawled_at: :desc)[0].crawled_at.to_i/HOUR)*HOUR).to_i
-    @updated_date = Time.at(@updated_date_timestamp).to_s
+    @updated_date= Time.at((State.all.where('official_flag is true').order(crawled_at: :desc)[0].crawled_at.to_i/HOUR)*HOUR).to_s
     render params[:page]
   end
 end
