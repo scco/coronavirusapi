89972504f594260b952cef9d16f4030183e0544d
Caleb Brown <calebcarltonbrown@gmail.com>
2020-03-26 11:01:17 -0400

    Adding last-crawled date to static pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
+  HOUR = 3600
   def show
+    @updated_date_timestamp = Time.at((State.all.where('official_flag is true').order(crawled_at: :desc)[0].crawled_at.to_i/HOUR)*HOUR).to_i
+    @updated_date = Time.at(@updated_date_timestamp).to_s
     render params[:page]
   end
 end
