5e12420ddb6a0ac23f436357169b28b850535bd4
Rudi Cilibrasi <cilibrar@gmail.com>
2020-03-28 22:02:06 -0700

    add sortable table by clicking column headers

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('datatables.net-bs4')
+
+import $ from 'jquery';
+global.$ = jQuery;
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
